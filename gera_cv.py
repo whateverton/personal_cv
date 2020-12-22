ff404f57710e39e0cd97e867d1ed592d17cb00ab
whateverton <evertonandradevilela@gmail.com>
2020-12-21 21:21:28 -0300

    Changing output folder, it is more organized now

--- gera_cv.py
+++ gera_cv.py
@@ -5,19 +5,18 @@ import os
 import subprocess
 from shutil import copyfile
 
-import pdb; pdb.set_trace()
-
 if __name__ == '__main__':
     languages = ['portuguese', 'english', 'french']
     name = 'EvertonAndradeVilela'
 
     CV_EXT = '.pdf'
     CV_FILE_BASE = name + '-'
+    CV_OUTPUT_FOLDER = 'output\\'
 
     for language in languages:
         print ('Generating ' + language.capitalize() + ' CV'+ '...')
 
-        subprocess.check_output(['pdflatex', '"\def\doclanguage{' + language + '} \input{main.tex}"', '-output-directory=' + language, '-jobname=' + CV_FILE_BASE + language])
-        copyfile (language + '\\' + CV_FILE_BASE + language + CV_EXT, CV_FILE_BASE + language + CV_EXT)
+        subprocess.check_output(['pdflatex', '"\def\doclanguage{' + language + '} \input{main.tex}"', '-output-directory=' + CV_OUTPUT_FOLDER + language, '-jobname=' + CV_FILE_BASE + language])
+        copyfile (CV_OUTPUT_FOLDER + language + '\\' + CV_FILE_BASE + language + CV_EXT, CV_OUTPUT_FOLDER + CV_FILE_BASE + language + CV_EXT)
         
         print (language.capitalize() + ' CV generated successfully!\n')
